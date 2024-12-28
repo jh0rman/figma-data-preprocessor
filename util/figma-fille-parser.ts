3641b0ae54f306f5da97771a36720717378c7c5c
Jhorman Tito <jhormanrus@gmail.com>
2024-12-27 20:47:22 -0500

    add figma file parser

---
+++ util/figma-fille-parser.ts
@@ -0,0 +1,38 @@
+export class FigmaFileParser {
+  rootNodes: Record<string, any>[]
+  prototypeStartNodeID: string | null = null
+
+  constructor(private file: Record<string, any>) {
+    const canvasNodes = this.file.document.children || []
+    this.rootNodes = canvasNodes[0].children || []
+    this.prototypeStartNodeID = canvasNodes[1].prototypeStartNodeID
+    if (!this.prototypeStartNodeID) {
+      throw new Error('Prototype start node ID not found')
+    }
+  }
+
+  cleanData() {
+    const result = []
+    result.push(...this.rootNodes.map(n => this.traverse(n)).filter(Boolean))
+    return result
+  }
+
+  private traverse(node: Record<string, any>) {
+    const element: Record<string, any> = {}
+
+    if (node.type === 'TEXT') {
+      element.type = 'text'
+      element.content = node.characters
+      element.style = node.style
+    } else if (node.type === 'FRAME' || node.type === 'GROUP') {
+      element.type = 'container'
+      element.name = node.name
+      element.children = node.children ? node.children.map(this.traverse).filter(Boolean) : []
+    } else if (node.type === 'RECTANGLE') {
+      element.type = 'box'
+      element.backgroundColor = node.backgroundColor
+    }
+
+    return element.type ? element : null
+  }
+}
